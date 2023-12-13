0f7811d8f66fe5b455d61ef74d596ce3dde3dd14
Brun00liveira <brunobromo321@gmail.com>
2023-12-13 18:32:20 -0300

    teste de erro

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,17 +4,5 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-
-class AdminMiddleware
-{
-    public function handle($request, Closure $next)
-    {
-        return Redirect::route('login')->withErrors(['alert' => 'Você precisa estar logado como administrador para acessar essa página']);
-asdgsdfgasd
-fgasdgasd
-        // Verifique aqui se o usuário é um administrador e faça o redirecionamento caso necessário
-
-        return $next($request);
-    }
-}
+use gqger\Support\Facades\Redirect;
+asdgqwergewrrgxcvcvasfc sedf f d f
